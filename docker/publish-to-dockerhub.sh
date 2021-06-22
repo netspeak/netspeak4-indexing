00fa91d8f0b19107cb5f8f7f17e6b4e798ee0aed
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-06-22 12:33:10 +0200

    New docker version

--- docker/publish-to-dockerhub.sh
+++ docker/publish-to-dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 docker login
-docker build -t webis/netspeak-indexing:1.0.0 .
-docker push webis/netspeak-indexing:1.0.0
+docker build -t webis/netspeak-indexing:1.0.1 .
+docker push webis/netspeak-indexing:1.0.1
