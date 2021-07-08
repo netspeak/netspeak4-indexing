f53bdd2eb3013759ff54280cf278e43e44f575db
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-07-08 14:29:48 +0200

    Released Docker image v1.0.3

--- docker/publish-to-dockerhub.sh
+++ docker/publish-to-dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 docker login
-docker build -t webis/netspeak-indexing:1.0.2 .
-docker push webis/netspeak-indexing:1.0.2
+docker build -t webis/netspeak-indexing:1.0.3 .
+docker push webis/netspeak-indexing:1.0.3
